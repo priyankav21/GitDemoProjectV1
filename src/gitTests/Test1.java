f9b7a89419e5b0ac9cb7ceb77a87aa3f70f79c60
Priyanka V <priyanka2000vm@gmail.com>
2022-02-22 14:31:02 +0530

    Commit the Base repo..

---
+++ src/gitTests/Test1.java
@@ -0,0 +1,14 @@
+package gitTests;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String Pass = "yes";
+		
+		if(Pass == "yes")
+			System.out.println("Great! Go for next");
+		else
+			System.out.println(" Work on it");
+	}
+}
